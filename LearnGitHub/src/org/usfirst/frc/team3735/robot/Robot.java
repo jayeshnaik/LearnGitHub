a73c89dbfa0de435d98c21a1814b8ef2e8385721
Rohan Koripalli <rohan@koripalli.com>
2017-01-29 20:10:30 -0600

    Just Another

--- LearnGitHub/src/org/usfirst/frc/team3735/robot/Robot.java
+++ LearnGitHub/src/org/usfirst/frc/team3735/robot/Robot.java
@@ -28,7 +28,6 @@ public class Robot extends SampleRobot {
 	Joystick leftStick = new Joystick(0); // set to ID 1 in DriverStation
 	Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
 	
-	// More Comments
 
 	public Robot() {
 		myRobot.setExpiration(0.2);
